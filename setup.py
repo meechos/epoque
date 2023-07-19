b9bd83374f289ae1ff7bebc0f32c648c1e4c71a3
Dimitris Fotis Sakellariou <diastro@email.com>
2023-07-19 17:26:17 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='epoque',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='me',
+    author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
